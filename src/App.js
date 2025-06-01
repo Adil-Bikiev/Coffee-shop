0b9c54b4fb3dbce748edcfbd0fdb86cc9af56a93
EMMMABK <adilbikiev2@gmail.com>
2025-06-01 23:30:00 +0600

    add Home Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import './App.css'
 import Header from './components/header/Header'
+import Home from './components/home/Home'
 
 export default function App() {
   console.log('App rendered'); // <-- Это должно появиться в консоли
 
   return (
-    
     <>
       <Header/>
+      <Home/>
     </>
   )
 }
