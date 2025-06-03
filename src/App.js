db182abf07c557ee51b8d92c34bfbd7bdf683ed8
EMMMABK <adilbikiev2@gmail.com>
2025-06-03 22:15:43 +0600

    add Menu Component

--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@ import Header from './components/header/Header'
 import Home from './components/home/Home'
 import About from './components/about/About'
 import Features from './components/features/Features'
+import Menu from './components/menu/Menu'
 
 export default function App() {
-  console.log('App rendered'); // <-- Это должно появиться в консоли
-
   return (
     <>
       <Header/>
       <Home/>
       <About/>
       <Features/>
+      <Menu/>
     </>
   )
 }
