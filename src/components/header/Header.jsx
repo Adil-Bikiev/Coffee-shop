abc43abd731f551f543d62b4b06f123341ac4cf1
EMMMABK <adilbikiev2@gmail.com>
2025-06-07 00:50:38 +0600

    add Header Scroll

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,35 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from '../../assets/logo.png'
 import { links } from '../../Data'
+import { Link } from 'react-scroll';
 import { FaStream } from "react-icons/fa";
+import { animateScroll } from 'react-scroll';
 import './header.css'
 
 const Header = () => {
-  return (
-    <header className="header">
-        <nav className="nav container">
-            <a href="/" className="nav__logo">
-                <img src={logo} alt="" className='nav__logo-img'/>
-            </a>
+    const [scrollHeader, setScrollHeader] = useState(false);
 
-            <div className="nav__menu">
-                <ul className="nav__list">
-                    {links.map(({name, path}, index) => {
-                        return (
-                            <li className="nav__item" key={index}>
-                                <a href={path} className="nav__link">
-                                    {name}
-                                </a>
-                            </li>
-                        )
-                    })}
-                </ul>
-            </div>
-            <div className="nav__toogle">
-                <FaStream/>
-            </div>
-        </nav>
-    </header>
+    const changeHeader = () => {
+        if(window.scrollY >= 80){
+            setScrollHeader(true)
+        }else{
+            setScrollHeader(false)
+        }
+    }
+
+    const scrollTop = () => {
+        animateScroll.scrollToTop();
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', changeHeader )
+    }, [])
+    return (
+        <header className={`${scrollHeader ? 'scroll-header' : ''} header `}>
+            <nav className="nav container">
+                <Link to="/" onClick={scrollTop} className="nav__logo">
+                    <img src={logo} alt="" className='nav__logo-img'/>
+                </Link>
+
+                <div className="nav__menu">
+                    <ul className="nav__list">
+                        {links.map(({name, path}, index) => {
+                            return (
+                                <li className="nav__item" key={index}>
+                                    <Link spy={true} smooth={true} offset={-60} hashSpy={true} duration={500} to={path} href={path} className="nav__link">
+                                        {name}
+                                    </Link>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </div>
+                <div className="nav__toogle">
+                    <FaStream/>
+                </div>
+            </nav>
+        </header>
   );
 };
 
